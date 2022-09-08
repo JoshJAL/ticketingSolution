a9b954220f071a5dc8e98de95ec5e2019fc80016
JoshRGE <joshl@robgrahamenterprises.com>
2022-09-08 15:48:35 -0400

    added proper background color for buttons to be consistent over all web browsers

--- styles/palette.ts
+++ styles/palette.ts
@@ -2,6 +2,7 @@ const palette = {
   inputBackgroundColor: "#b3b3b3",
   pageBackgroundColor: "rgb(235, 235, 235)",
   emergencyRed: "#a60505",
+  buttonColor: "#6b6b6b"
 };
 
 export default palette;

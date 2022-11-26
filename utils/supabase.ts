0b777e7e3069d0c8be81f181a7d5a146ab9628a6
Derek Rodriguez <drod2169@gmail.com>
2022-11-25 22:30:25 -0500

    Add footer component

---
+++ utils/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// it'll never be null, but let's make it happy without non-null assertions
+export default createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+);

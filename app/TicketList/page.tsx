24aa36553b13fe031b8195ff643205206e0b83c7
Derek Rodriguez <drod2169@gmail.com>
2022-11-26 01:08:45 -0500

    Update query with join

--- app/TicketList/page.tsx
+++ app/TicketList/page.tsx
@@ -1,11 +1,25 @@
 import supabase from '@/utils/supabase';
 import TicketCard from '@/components/Ticket/TicketCard';
+import { Ticket } from '@/types/ticket';
 
 export const revalidate = 0;
 
 export default async function TicketListPage() {
   // TODO: when auth is added, filter by signed in user
-  const { data: tickets } = await supabase.from('tickets').select('*');
+  const { data: tickets }: { data: Ticket[] | null } = await supabase.from('tickets').select(`
+    id,
+    title,
+    description,
+    picture,
+    priority_level,
+    status,
+    assigned_to,
+    complexity_level,
+    reviewed_by,
+    notes,
+    created_by,
+    ticketType ( value )
+  `);
   if (!tickets) {
     return <p>No tickets found</p>;
   }
